55c611c7d1410df2c09628923eed7fc54014f6ab
Daniel Weibel <danielmweibel@gmail.com>
2018-08-16 14:19:13 +0800

    Add component architecture diagram

--- docker-run.sh
+++ docker-run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-TAG=${1:-core-0.0.1}
+TAG=${1:-cli-0.0.1}
 
 # Set values before executing
 AMQP_URI=
 
 docker run --rm -ti \
+  --name tg-monitor-cli \
   -e AMQP_URI="$AMQP_URI" \
+  -v /var/log:/var/log/tg-monitor \
   weibeld/tg-monitor:"$TAG"
